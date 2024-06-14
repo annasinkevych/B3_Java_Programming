19b1cbb4ca59058645f85c600b1de61d8e44142e
Anna Nicol <an.annanicol@gmail.com>
2024-06-13 22:21:30 -0400

    interface_polymorphism

---
+++ src/day38_a_abstraction_interface/animal/Bird.java
@@ -0,0 +1,9 @@
+package day38_a_abstraction_interface.animal;
+
+public abstract class Bird extends Animal{
+
+    public void hasWings(){
+        System.out.println("Bird's has wings");
+    }
+
+}
